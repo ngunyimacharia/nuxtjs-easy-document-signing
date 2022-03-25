b14b1059bcc25416b1a8c334c13a6eb5ba1414b5
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-03-25 12:15:52 +0200

    Allow sample usage and navigate to next page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <AppHeader />
-    <div class="grid grid-cols-2">
-      <form @submit.prevent="upload">
+    <div class="grid grid-cols-3 gap-10">
+      <form @submit.prevent="upload" class="col-span-2">
         <div class="shadow sm:rounded-md sm:overflow-hidden">
           <div class="bg-white py-6 px-4 space-y-6 sm:p-6">
             <div>
@@ -19,11 +19,18 @@
           <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
             <p class="text-muted text-sm text-center" v-if="uploading">Uploading... </p>
             <button v-else type="submit" class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Save
+              Select
             </button>
           </div>
         </div>
       </form>
+      <div class="shadow sm:rounded-md sm:overflow-hidden cursor-pointer" @click="useSample">
+        <div class="bg-white py-6 px-4 space-y-6 sm:p-6 text-center">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">Use pre-loaded PDF</h3>
+            <p class="mt-1 text-sm text-gray-500">Click here to proceed with an already existing PDF</p>
+            <cld-image :public-id="samplePdf" class="mx-auto" width="200" height="200" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +39,7 @@
 export default {
   data(){
     return {
+      sampleDocPublicId:'nuxtjs-easy-document-signing/sample/DuckDuckGo_Privacy_simplified',
       file:null,
       uploading:false,
       cloudinaryFile:null,
@@ -58,6 +66,9 @@ export default {
       this.uploading = false;
       this.$router.push(`/sign?image_public_id=${this.cloudinaryFile.public_id}` )
     },
+    useSample(){
+      this.$router.push(`/sign?image_public_id=${this.sampleDocPublicId}` )
+    }
   }
 }
 </script>
