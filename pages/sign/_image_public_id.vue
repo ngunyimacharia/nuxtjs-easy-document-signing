0a0aeaeaa5650506eb727510c41c67f39a7cb276
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-03-25 13:04:20 +0200

    Sign page complete

--- pages/sign/_image_public_id.vue
+++ pages/sign/_image_public_id.vue
@@ -1,12 +1,41 @@
 <template>
-  <section class="sign-container">
-    <div class="wrapper">
-      <img :src="imageUrl" height="800" width="600"/>
-      <canvas id="signature-pad" height="800" width="600"></canvas>
+  <div>
+    <AppHeader :currentStep="1"/>
+
+    <div class="text-center my-10 ">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">Sign PDF Document</h3>
+          <p class="mt-1 text-sm text-gray-500">Feel free to draw your signature anywhere on the document below</p>
     </div>
-    <button @click="save">Save</button>
-    <button @click="clear">Clear</button>
-  </section>
+
+    <section class="sign-container">
+      <div class="wrapper border">
+        <img :src="imageUrl" height="800" width="600"/>
+        <canvas id="signature-pad" height="800" width="600"></canvas>
+      </div>
+    </section>
+
+    <div class="my-10 text-center">
+      <p class="text-muted text-sm text-center" v-if="uploading">Generating Signature & Uploading... </p>
+      <span v-else>
+        <button 
+          type="button" 
+          @click="save"
+          class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Save
+        </button>
+        
+        <button 
+          type="button" 
+          @click="clear"
+          class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Clear
+        </button>
+      </span>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -22,6 +51,7 @@ export default {
   data(){
     return {
       signaturePad:null,
+      uploading:false,
     }
   },
   mounted(){
@@ -37,14 +67,18 @@ export default {
       this.signaturePad = new SignaturePad(document.getElementById('signature-pad'));
     },
     async save(){
+        this.uploading = true;
+        
         var data = this.signaturePad.toDataURL('image/png');
 
-        const instance = await this.$cloudinary.upload(data, {
+        const signatureCloudinaryInstance = await this.$cloudinary.upload(data, {
           folder: 'nuxtjs-easy-document-signing',
           upload_preset: "default-preset",
         });
 
-        console.log(instance);
+        console.log(signatureCloudinaryInstance)
+
+        this.$router.push(`/sign/${encodeURIComponent(this.imagePublicId)}/preview/${encodeURIComponent(signatureCloudinaryInstance.public_id)}` )
     },
     clear(){
         this.signaturePad.clear();
@@ -57,7 +91,6 @@ export default {
 .sign-container{
   width:100vw;
   height:100vh;
-  background: rgba(0,0,0,0.1);
 }
 .sign-container .wrapper{
   height:800px;
